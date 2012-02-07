8be51a6119af20af4881107a5fb75efa97a176a3
Cássio Marques <cassiommc@gmail.com>
2012-02-06 23:25:59 -0200

    Correções no spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ require 'rack/test'
 ENV ||= {}
 ENV["SINATRA_ENV"] = "test"
 
-require File.dirname(__FILE__) + "/../iceleres_users"
+$:.unshift File.dirname(File.expand_path(__FILE__)) + "/lib"
+require "fake_braspag"
 Dir[File.dirname(__FILE__) + "/support/*.rb"].each { |f| require f }
 
 def app
