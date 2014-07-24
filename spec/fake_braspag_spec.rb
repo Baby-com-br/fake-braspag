fd0bee0dddfdbeb7508e53e203b27f055f4920da
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-07-24 17:23:01 -0300

    Generate a valid response

--- spec/fake_braspag_spec.rb
+++ spec/fake_braspag_spec.rb
@@ -4,6 +4,14 @@ describe FakeBraspag::Application do
   it 'responds to' do
     post '/webservices/pagador/Pagador.asmx/Capture'
 
-    expect(last_response.body).to eq "<em>hi</em>\n"
+    expect(last_response.body).to eq <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<PagadorReturn xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="https://www.pagador.com.br/webservice/pagador">
+  <amount>20.01</amount>
+  <message>F                 REDE                 @    CONFIRMACAO DE PRE-AUTORIZACAO    @COMPR:257575054    VALOR:        20,01@ESTAB:040187624 DINDA COM BR          @24.07.14-16:27:33 TERM:RO128278/528374@AUTORIZACAO EMISSOR: 642980           @CODIGO PRE-AUTORIZACAO: 52978         @CARTAO: xxxxxxxxxxxx4242              @     RECONHECO E PAGAREI A DIVIDA     @          AQUI REPRESENTADA           @@@     ____________________________     @@</message>
+  <returnCode>0</returnCode>
+  <transactionId>257575054</transactionId>
+</PagadorReturn>
+    XML
   end
 end
