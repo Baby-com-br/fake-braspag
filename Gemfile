c7eb8a96dd9839c9769ef25939dce2fa1ca96ee4
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-07-24 15:50:09 -0300

    :fire:

--- Gemfile
+++ Gemfile
@@ -2,23 +2,8 @@ source 'http://rubygems.org'
 
 ruby '2.1.2'
 
-gem "rake"
-gem "nokogiri"
-gem "rack", "1.2.3"
-gem "sinatra"
-gem "unicorn"
-gem "rbraspag"
-gem "settingslogic", "2.0.6"
-gem "cs-httpi", "0.9.5.2"
-gem "yajl-ruby"
-gem "redis"
-
+gem 'sinatra'
 
 group :test, :development do
-  gem "rspec"
-  gem 'rack-test', :require => "rack/test"
-end
-
-group :test do
-  gem "fakeredis", :require => "fakeredis/rspec"
+  gem 'rspec'
 end
--- lib/fake_braspag.rb
+++ lib/fake_braspag.rb
@@ -2,51 +2,16 @@ require "bundler/setup"
 
 ENV["RACK_ENV"] ||= "development"
 
-Bundler.require 
+Bundler.setup
+
+require 'sinatra'
 
 $: << File.dirname(__FILE__)
 
-require "order"
-require "credit_card"
-require "bill"
-require "eft"
-require "settings"
-
 module FakeBraspag
-  module PaymentType
-    CREDIT_CARD = 1
-    BILL        = 2
-    EFT         = 3
-  end
-    
   class App < Sinatra::Base
-    private
-    def change_bill_status(status)
-      Order.change_status params[:order_id], status
-    end
-
-    def pay_bill
-      change_bill_status Order::Status::PAID 
-    end
-
-    def cancel_bill
-      change_bill_status Order::Status::CANCELLED
-    end
-    
-    def crypt_value
-      Braspag::Crypto::JarWebservice.encrypt({
-        :VENDAID => params[:order_id]
-      })
-    end
-    
     configure do
       set :root, File.dirname(__FILE__)
-      set :views, settings.root + '/templates'
-      set :show_expections, false
-      register Order
-      register CreditCard
-      register Bill
-      register Eft
     end
   end
 end
