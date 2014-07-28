dee9880b08bdf039550e53dd1e18d351aac233f9
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-07-28 18:17:01 -0300

    Use the order object not the parameters

--- lib/fake_braspag.rb
+++ lib/fake_braspag.rb
@@ -14,8 +14,8 @@ require 'models/order'
 module FakeBraspag
   class Application < Sinatra::Base
     post '/webservices/pagador/Pagador.asmx/Authorize' do
-      Order.create params
-      builder :authorize_success, {}, { order: params }
+      order = Order.create params
+      builder :authorize_success, {}, { order: order }
     end
 
     post '/webservices/pagador/Pagador.asmx/Capture' do
