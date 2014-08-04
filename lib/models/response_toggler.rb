916a291aa366eb516dd67ddcb3a5a0e5059b5fae
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-08-04 14:39:50 -0300

    Extract a method to determine the key used for ResponseToggler

--- lib/models/response_toggler.rb
+++ lib/models/response_toggler.rb
@@ -13,16 +13,22 @@ class ResponseToggler
 
   # Public: Disable the response for the given `namespace`.
   def self.disable(namespace)
-    connection.set(KEY_PREFIX + namespace, true, ex: 60 * 60)
+    connection.set(key_for(namespace), true, ex: 60 * 60)
   end
 
   # Public: Enable the response for the given `namespace`.
   def self.enable(namespace)
-    connection.del(KEY_PREFIX + namespace)
+    connection.del(key_for(namespace))
   end
 
   # Public: Check if the response is enabled got the given `namespace`.
   def self.enabled?(namespace)
-    !connection.exists(KEY_PREFIX + namespace)
+    !connection.exists(key_for(namespace))
   end
+
+  # Internal: Returns the full key to be used on the persistence layer.
+  def self.key_for(namespace)
+    KEY_PREFIX + namespace.to_s
+  end
+  private_class_method :key_for
 end
