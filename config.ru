9e8cb50dd9c9b35885f886e59a573685f8930a35
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-08-08 14:35:54 -0300

    Add basic authentication to the toggler endpoint

--- config.ru
+++ config.ru
@@ -2,4 +2,6 @@ $:.unshift File.dirname(File.expand_path(__FILE__)) + '/lib'
 
 require 'fake_braspag'
 
+FakeBraspag.setup(ENV)
+
 run FakeBraspag.app
--- lib/fake_braspag.rb
+++ lib/fake_braspag.rb
@@ -20,6 +20,52 @@ Order.connection = connection
 ResponseToggler.connection = connection
 
 module FakeBraspag
+  @env = nil
+
+  # Public: The base exception class raised when errors are encountered.
+  class Error < StandardError; end
+
+  # Public: Setup the application.
+  #
+  # settings - Hash of app settings. Typically ENV but any object that responds
+  #            to #[], #[]= and #each is valid. See required_settings for
+  #            required keys. The RACK_ENV key is always required.
+  #
+  # Raises an Error when required settings are missing.
+  # Returns nothing.
+  def self.setup(settings)
+    @env = settings["RACK_ENV"]
+
+    if @env.nil? || @env.empty?
+      raise Error, "RACK_ENV is required"
+    end
+
+    required_settings.each do |setting|
+      next if !settings[setting].nil? && !settings[setting].empty?
+
+      if @env == "production"
+        raise Error, "#{setting} setting is required"
+      end
+    end
+
+    FakeBraspag::Toggler.set(
+      username: settings['FAKE_BRASPAG_TOGGLER_USERNAME'],
+      password: settings['FAKE_BRASPAG_TOGGLER_PASSWORD']
+    )
+  end
+
+  # Internal: The application environment.
+  def self.env
+    @env
+  end
+
+  # Internal: List of settings required in production.
+  #
+  # Returns an Array of Strings.
+  def self.required_settings
+    %w[FAKE_BRASPAG_TOGGLER_USERNAME FAKE_BRASPAG_TOGGLER_PASSWORD]
+  end
+
   # Public: The Fake Braspag Rack application, assembled from two apps.
   #
   # Returns a memoized Rack application.
@@ -30,6 +76,13 @@ module FakeBraspag
       end
 
       map '/' do
+        if FakeBraspag.env == 'production'
+          use Rack::Auth::Basic do |username, password|
+            username == FakeBraspag::Toggler.username &&
+              password == FakeBraspag::Toggler.password
+          end
+        end
+
         run FakeBraspag::Toggler
       end
     }
