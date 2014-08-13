23dbabd320c51c8c78e06fb3a9839bd4eb77c9a3
João Britto <jabcalves@gmail.com>
2014-08-13 19:04:26 -0300

    Parse the SOAP request into a Hash

--- lib/fake_braspag/credit_cards.rb
+++ lib/fake_braspag/credit_cards.rb
@@ -1,30 +1,48 @@
+require 'active_support/core_ext/hash/conversions'
+
 module FakeBraspag
   class CreditCards < Sinatra::Base
     post '/CartaoProtegido.asmx' do
-      xml = request.body.read
-      method = xml[ /<env:Body>\s*<tns:(.+?)>/, 1 ]
+      operation, data = parse_soap_request
 
-      case method
+      case operation
       when 'SaveCreditCard'
-        card = FakeBraspag::CreditCard.new(xml)
-
-        if card.valid?
-          builder :save_credit_card_success, locals: { card: card }
-        else
-          builder :save_credit_card_failure, locals: {
-            error_code: 732,
-            error_message: 'SaveCreditCardRequestId can not be null'
-          }
-        end
+        save_credit_card(data['saveCreditCardRequestWS'])
       when 'JustClickShop'
         # TODO
       else
-        # TODO
+        # TODO 400 - bad request, unsupported method
       end
     end
 
     get '/CartaoProtegido.asmx' do
       erb :protected_credit_card_wsdl, format: :xml
+    end
+
+    private
+
+    # TODO: doc
+    # TODO: handle invalid input
+    def parse_soap_request
+      soap_request = Hash.from_xml(request.body.read)
+      request_data = soap_request['Envelope']['Body']
+      operation = request_data.keys.first
+
+      [ operation, request_data[operation] ]
+    end
+
+    # TODO: doc
+    def save_credit_card(credit_card_params)
+      card = FakeBraspag::CreditCard.new(credit_card_params)
+
+      if card.valid?
+        builder :save_credit_card_success, locals: { card: card }
+      else
+        builder :save_credit_card_failure, locals: {
+          error_code: 732,
+          error_message: 'SaveCreditCardRequestId can not be null'
+        }
+      end
     end
   end
 end
--- lib/models/credit_card.rb
+++ lib/models/credit_card.rb
@@ -4,51 +4,63 @@ module FakeBraspag
   #
   # Examples
   #
-  #   card = CreditCard.new(braspag_request.body_as_xml)
+  #   card = CreditCard.new(
+  #     'RequestId'      => '{BF4616EA-448A-4A15-9590-CE1163F3AD50}'
+  #     'MerchantKey'    => '{84BE7E7F-698A-6C74-F820-AE359C2A07C2}'
+  #     'CustomerName'   => 'John'
+  #     'CardHolder'     => 'John Doe'
+  #     'CardNumber'     => '4111111111111111'
+  #     'CardExpiration' => '05/2017'
+  #   )
+  #
   #   card.correlation_id
   #   # => "bf4616ea-448a-4a15-9590-ce1163f3ad50"
+  #
   #   card.just_click_key
   #   # => "370a5342-c97a-4e55-8157-95c23fe18d03"
   class CreditCard
-    # Public: Random key used to identify a customer-card pair uniquely.
-    attr_reader :just_click_key
-
-    # Public: A transaction key matching the request `RequestId` to the response.
-    attr_reader :correlation_id
-
     # Public: Initialize a CreditCard.
     #
-    # xml - XML content of a SOAP `CreditCardSave` request envelope.
-    def initialize(xml)
-      @correlation_id = request_id(xml)
-      @just_click_key = SecureRandom.uuid
+    # attributes - A hash containing card information such as number and expiration.
+    def initialize(attributes)
+      request_id = uuid(attributes['RequestId'])
+
+      @attributes = {
+        'JustClickKey' => SecureRandom.uuid,
+        'CorrelationId' => request_id
+      }
+    end
+
+    # Public: Random UUID key used to identify a customer-card pair uniquely.
+    def just_click_key
+      @attributes['JustClickKey']
+    end
+
+    # Public: A transaction UUID key, matching the request `RequestId` to the response.
+    def correlation_id
+      @attributes['CorrelationId']
     end
 
     # Public: Determine if this is a valid credit card.
     #
-    # A valid credit card XML request must contain a
+    # A valid credit card save request must contain a
     # `RequestId`, so a `CorrelationId` can be returned.
-    #
-    # Returns true when the XML is well-formed and false otherwise.
     def valid?
-      @correlation_id
+      correlation_id
     end
 
     private
 
-    # Internal: Retrive the `RequestId` value from a XML input.
+    # Private: Normalize a UUID value.
     #
-    # xml - The XML String to be searched.
+    # Wrapping characters are removed and the output is downcased.
     #
     # Examples
     #
-    #   request_id("...<tns:RequestId>{BE54D851-9B9C-4FD2-8846-F043166D60C1}<tns:RequestId>...")
-    #   # => "BE54D851-9B9C-4FD2-8846-F043166D60C1"
-    #
-    # Returns a UUID String or nil if there is no `RequestId` tag in the XML.
-    def request_id(xml)
-      id = xml[ /<tns:RequestId>\s*{?(.+?)}?\s*<\//, 1 ]
-      id.downcase if id
+    #   uuid('{BF4616EA-448A-4A15-9590-CE1163F3AD50}')
+    #   # => 'bf4616ea-448a-4a15-9590-ce1163f3ad50'
+    def uuid(value)
+      value[/[\h-]+/].downcase if value
     end
   end
 end
