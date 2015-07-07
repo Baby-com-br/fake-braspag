89d6d2a6c41120541c27c2d97f7acb98c99c7c74
Johalf Farina <johalffarina@gmail.com>
2015-07-07 14:10:31 -0300

    mocking missing credit cards spec redis calls

--- spec/credit_cards_spec.rb
+++ spec/credit_cards_spec.rb
@@ -15,7 +15,7 @@ describe FakeBraspag::CreditCards do
 
   describe 'SaveCreditCard' do
     context 'when the response is enabled' do
-      before { ResponseToggler.enable('save_credit_card') }
+      before { allow(ResponseToggler).to receive(:enabled?).with('save_credit_card').and_return(true) }
 
       it 'renders a successful response with a valid just click key and a correlation id' do
         request_id = 'bf4616ea-448a-4a15-9590-ce1163f3ad50'
@@ -158,7 +158,7 @@ describe FakeBraspag::CreditCards do
 
   describe 'JustClickShop' do
     context 'when the response is enabled' do
-      before { ResponseToggler.enable('just_click_shop') }
+      before { allow(ResponseToggler).to receive(:enabled?).with('just_click_shop').and_return(true) }
 
       it 'renders a successful response and saves a captured order' do
         request_id = 'bf4616ea-448a-4a15-9590-ce1163f3ad50'
@@ -325,7 +325,7 @@ describe FakeBraspag::CreditCards do
 
   describe 'GetCreditCard' do
     context 'when the response is enabled' do
-      before { ResponseToggler.enable('get_credit_card') }
+      before { allow(ResponseToggler).to receive(:enabled?).with('get_credit_card').and_return(true) }
 
       it 'returns a valid credit card result' do
         post 'FakeCreditCard/CartaoProtegido.asmx', <<-XML.strip_heredoc
